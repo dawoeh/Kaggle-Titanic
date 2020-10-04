4b6b2787031379fe33045564f61303202ed27242
David Wöhlert <david.woehlert@gmx.de>
2020-10-04 13:23:03 +0200

    Ticket group, Fare group, Random Forest Classification

--- train.py
+++ train.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 import seaborn as sn
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
 from sklearn import preprocessing 
+from sklearn import metrics
 
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
@@ -16,16 +18,18 @@ test['Age'] = test['Age'].replace('NaN', np.nan)
 
 titels = ('Mlle.', 'Dr.', 'Master.', 'Col.', 'Rev.', 'Sir.', 'Lady.', 'Major.', 'Mme.', 'Capt.')
 
-k=-1
-for i in train['Age']:
-	k+=1
+print(train.groupby('SibSp', as_index=False)['Age'].mean())
+print(train.groupby('Parch', as_index=False)['Age'].mean())
+
+k=0
+for i in train['PassengerId']:
 	string = (train.iloc[k,3])
 	train.at[k, 'Name'] = string.split(',', 1)[0]
 	train.at[k, 'Titles'] = 0
 	for i in string.split(' '):
 		if i in titels:
 			train.at[k, 'Titles'] = 1	
-	if pd.isna(i):
+	if pd.isna(train.at[k, 'Age']):
 		if train.iloc[k,6] < 2:
 			if train.iloc[k,7] > 2:
 				train.at[k, 'Age'] = train.loc[train['Parch'] > 2, 'Age'].mean()
@@ -36,16 +40,34 @@ for i in train['Age']:
 		if train.iloc[k,6] > 2:
 			train.at[k, 'Age'] = train.loc[train['SibSp'] > 2, 'Age'].mean()
 
-k=-1
-for i in test['Age']:
+	try:
+		integer = int(train.iloc[k,8])
+		if integer > 99999:
+			train.at[k, 'Ticket_group'] = 0
+		if integer < 100000:
+			train.at[k, 'Ticket_group'] = 1
+	except ValueError:
+		train.at[k, 'Ticket_group'] = 2
+
+	if train.iloc[k,9] < 8:
+		train.at[k, 'Fare_group'] = 0
+	elif train.iloc[k,9] < 16:
+		train.at[k, 'Fare_group'] = 1
+	elif train.iloc[k,9] < 50:
+		train.at[k, 'Fare_group'] = 2
+	else:
+		train.at[k, 'Fare_group'] = 3
 	k+=1
+
+k=0
+for i in test['PassengerId']:
 	string = (test.iloc[k,2])
 	test.at[k, 'Name'] = string.split(',', 1)[0]
 	test.at[k, 'Titles'] = 0
 	for i in string.split(' '):
 		if i in titels:
 			test.at[k, 'Titles'] = 1	
-	if pd.isna(i):
+	if pd.isna(test.at[k, 'Age']):
 		if test.iloc[k,5] < 2:
 			if test.iloc[k,6] > 2:
 				test.at[k, 'Age'] = train.loc[train['Parch'] > 2, 'Age'].mean()
@@ -55,13 +77,28 @@ for i in test['Age']:
 			test.at[k, 'Age'] = train.loc[train['SibSp'] == 2, 'Age'].mean()
 		if test.iloc[k,5] > 2:
 			test.at[k, 'Age'] = train.loc[train['SibSp'] > 2, 'Age'].mean()
+	try:
+		integer = int(test.iloc[k,7])
+		if integer > 99999:
+			test.at[k, 'Ticket_group'] = 0
+		if integer < 100000:
+			test.at[k, 'Ticket_group'] = 1
+	except ValueError:
+		test.at[k, 'Ticket_group'] = 2
 
-print(train.groupby('SibSp', as_index=False)['Age'].mean())
-print(train.groupby('Parch', as_index=False)['Age'].mean())
+	if test.iloc[k,8] < 8:
+		test.at[k, 'Fare_group'] = 0
+	elif test.iloc[k,8] < 16:
+		test.at[k, 'Fare_group'] = 1
+	elif test.iloc[k,8] < 50:
+		test.at[k, 'Fare_group'] = 2
+	else:
+		test.at[k, 'Fare_group'] = 3
+	k+=1
 
 
-train['Age_bins'] = pd.cut(x=train['Age'], bins=[0, 18, 24, 30, 50, 80], labels=False)
-test['Age_bins'] = pd.cut(x=test['Age'], bins=[0, 18, 24, 30, 50, 80], labels=False)
+train['Age_bins'] = pd.cut(x=train['Age'], bins=[0, 4, 16, 24, 30, 40, 80], labels=False)
+test['Age_bins'] = pd.cut(x=test['Age'], bins=[0, 4, 16, 24, 30, 40, 80], labels=False)
 
 ##print(correlation)
 
@@ -76,12 +113,15 @@ plt.savefig('graphs/age_hist.png')
 train[['Age_bins']].hist(bins=5)
 plt.savefig('graphs/age_bins_hist.png')
 
-train = train.drop(["PassengerId","Cabin","Ticket","Name","Fare"], axis=1)
-test = test.drop(["PassengerId","Cabin","Ticket","Name","Fare"], axis=1)
+train = train.drop(["PassengerId","Cabin","Ticket","Name","Fare","Age"], axis=1)
+test = test.drop(["PassengerId","Cabin","Ticket","Name","Fare","Age"], axis=1)
 
 plt.close
 
 trainMatrix = train.corr()
+
+f, ax = plt.subplots(figsize=(11, 9))
+
 sn.heatmap(trainMatrix, annot=True)
 plt.savefig('graphs/heatmap.png')
 plt.close
@@ -89,10 +129,10 @@ x=0
 
 #####deal with nan
 
-#print(pd.isnull(train).sum() > 0)
-#print(pd.isnull(test).sum() > 0)
+print(pd.isnull(train).sum() > 0)
+print(pd.isnull(test).sum() > 0)
 
-columns_nan = ('Age','Age_bins','Embarked')
+columns_nan = ('Age_bins','Embarked')
 
 for column in columns_nan:
 	if column == 'Embarked':
@@ -122,6 +162,8 @@ test['Embarked']= label_encoder.fit_transform(test['Embarked'])
 X_train = train.drop("Survived", axis=1)
 Y_train = train["Survived"]
 X_test  = test.copy()
+Y_test = pd.read_csv("gender_submission.csv")
+Y_test = Y_test.drop("PassengerId", axis=1)
 
 #fit data
 
@@ -129,4 +171,9 @@ logreg = LogisticRegression(max_iter=1000)
 logreg.fit(X_train, Y_train)
 Y_pred = logreg.predict(X_test)
 acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
-print('Accuracy:',acc_log)
+print('Accuracy Linear Regression:',acc_log)
+
+clf=RandomForestClassifier(n_estimators=100)
+clf.fit(X_train,Y_train)
+Y_pred=clf.predict(X_test)
+print("Accuracy Random Forest:",metrics.accuracy_score(Y_test, Y_pred))
