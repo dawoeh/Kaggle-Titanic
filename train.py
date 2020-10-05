78edf929a4a78eb383dac110d9096ae777f98f5a
David Wöhlert <david.woehlert@gmx.de>
2020-10-05 22:54:04 +0200

    Cleanup

--- train.py
+++ train.py
@@ -3,33 +3,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sn
 
+from collections import Counter
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
 from sklearn import preprocessing 
 from sklearn import metrics
 
+#####import data
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
-IDtest = test["PassengerId"]
 
+#####Fill empty cells with nan
 train['Age'] = train['Age'].replace('NaN', np.nan)
 test['Age'] = test['Age'].replace('NaN', np.nan)
 
-titels = ('Mlle.', 'Dr.', 'Master.', 'Col.', 'Rev.', 'Sir.', 'Lady.', 'Major.', 'Mme.', 'Capt.')
+#####Show data statistics
+print(train.describe())
+print(test.describe())
 
+###dataset statistics for age and fare, since data are missing
 print(train.groupby('SibSp', as_index=False)['Age'].mean())
 print(train.groupby('Parch', as_index=False)['Age'].mean())
+print(train.groupby('Pclass', as_index=False)['Fare'].mean())
 
+#####count titles
+titles = []
+for i in train['Name']:
+	string = i.split(',', 1)[1]
+	titles.append(string.split('.')[0])
+print(Counter(titles))
+titels = ('Master.','Dr.','Rev.','Major.','Mlle.','Col.','Don.','Mme.','Lady.','Sir.','Capt.', 'the Countess.', 'Jonkheer.')
+
+######Fill empty numerical values and transform ticket into groups
 k=0
 for i in train['PassengerId']:
 	string = (train.iloc[k,3])
 	train.at[k, 'Name'] = string.split(',', 1)[0]
 	train.at[k, 'Titles'] = 0
-	for i in string.split(' '):
+
+	for i in string.split(' '):		###extract titels from names
 		if i in titels:
-			train.at[k, 'Titles'] = 1	
-	if pd.isna(train.at[k, 'Age']):
+			train.at[k, 'Titles'] = 1
+
+	if pd.isna(train.at[k, 'Age']):			###deal with unknown age
 		if train.iloc[k,6] < 2:
 			if train.iloc[k,7] > 2:
 				train.at[k, 'Age'] = train.loc[train['Parch'] > 2, 'Age'].mean()
@@ -40,7 +58,15 @@ for i in train['PassengerId']:
 		if train.iloc[k,6] > 2:
 			train.at[k, 'Age'] = train.loc[train['SibSp'] > 2, 'Age'].mean()
 
-	try:
+	if pd.isna(train.at[k, 'Fare']):		###deal with unknown fare
+		if train.at[k, 'Pclass'] == 1:
+			train.at[k, 'Fare'] = train.loc[train['Pclass'] == 1, 'Fare'].mean()
+		elif train.at[k, 'Pclass'] == 2:
+			train.at[k, 'Fare'] = train.loc[train['Pclass'] == 2, 'Fare'].mean()
+		else:
+			train.at[k, 'Fare'] = train.loc[train['Pclass'] == 3, 'Fare'].mean()
+
+	try:			####transform tickets into groups
 		integer = int(train.iloc[k,8])
 		if integer > 99999:
 			train.at[k, 'Ticket_group'] = 0
@@ -48,15 +74,6 @@ for i in train['PassengerId']:
 			train.at[k, 'Ticket_group'] = 1
 	except ValueError:
 		train.at[k, 'Ticket_group'] = 2
-
-	if train.iloc[k,9] < 8:
-		train.at[k, 'Fare_group'] = 0
-	elif train.iloc[k,9] < 16:
-		train.at[k, 'Fare_group'] = 1
-	elif train.iloc[k,9] < 50:
-		train.at[k, 'Fare_group'] = 2
-	else:
-		train.at[k, 'Fare_group'] = 3
 	k+=1
 
 k=0
@@ -64,10 +81,12 @@ for i in test['PassengerId']:
 	string = (test.iloc[k,2])
 	test.at[k, 'Name'] = string.split(',', 1)[0]
 	test.at[k, 'Titles'] = 0
-	for i in string.split(' '):
+
+	for i in string.split(' '):			###extract titels from names
 		if i in titels:
-			test.at[k, 'Titles'] = 1	
-	if pd.isna(test.at[k, 'Age']):
+			test.at[k, 'Titles'] = 1
+
+	if pd.isna(test.at[k, 'Age']):		###deal with unknown age
 		if test.iloc[k,5] < 2:
 			if test.iloc[k,6] > 2:
 				test.at[k, 'Age'] = train.loc[train['Parch'] > 2, 'Age'].mean()
@@ -77,7 +96,16 @@ for i in test['PassengerId']:
 			test.at[k, 'Age'] = train.loc[train['SibSp'] == 2, 'Age'].mean()
 		if test.iloc[k,5] > 2:
 			test.at[k, 'Age'] = train.loc[train['SibSp'] > 2, 'Age'].mean()
-	try:
+
+	if pd.isna(test.at[k, 'Fare']):		###deal with unknown fare
+		if test.at[k, 'Pclass'] == 1:
+			test.at[k, 'Fare'] = train.loc[train['Pclass'] == 1, 'Fare'].mean()
+		elif test.at[k, 'Pclass'] == 2:
+			test.at[k, 'Fare'] = train.loc[train['Pclass'] == 2, 'Fare'].mean()
+		else:
+			test.at[k, 'Fare'] = train.loc[train['Pclass'] == 3, 'Fare'].mean()
+
+	try:		####transform tickets into groups
 		integer = int(test.iloc[k,7])
 		if integer > 99999:
 			test.at[k, 'Ticket_group'] = 0
@@ -85,36 +113,32 @@ for i in test['PassengerId']:
 			test.at[k, 'Ticket_group'] = 1
 	except ValueError:
 		test.at[k, 'Ticket_group'] = 2
-
-	if test.iloc[k,8] < 8:
-		test.at[k, 'Fare_group'] = 0
-	elif test.iloc[k,8] < 16:
-		test.at[k, 'Fare_group'] = 1
-	elif test.iloc[k,8] < 50:
-		test.at[k, 'Fare_group'] = 2
-	else:
-		test.at[k, 'Fare_group'] = 3
 	k+=1
 
+#####create bins for age and fare
+ageb=(0, 4, 16, 24, 30, 40, 80) ###bins for age
+train['Age_bins'] = pd.cut(x=train['Age'], bins=ageb, labels=False)
+test['Age_bins'] = pd.cut(x=test['Age'], bins=ageb, labels=False)
 
-train['Age_bins'] = pd.cut(x=train['Age'], bins=[0, 4, 16, 24, 30, 40, 80], labels=False)
-test['Age_bins'] = pd.cut(x=test['Age'], bins=[0, 4, 16, 24, 30, 40, 80], labels=False)
+fareb=(-np.inf, 8.0, 16.0, 50.0, 1000) ###bins for fare
+train['Fare_bins'] = pd.cut(x=train['Fare'], include_lowest=True, bins=fareb, labels=False)
+test['Fare_bins'] = pd.cut(x=test['Fare'], include_lowest=True, bins=fareb, labels=False)
 
-##print(correlation)
-
-print(train.describe())
-
-#Create Graphs and Heatmap for data
-
+#####Create Graphs and Heatmap for data
 train[['Age']].hist(bins=10)
 plt.savefig('graphs/age_hist.png')
 
-
-train[['Age_bins']].hist(bins=5)
+train[['Age_bins']].hist(bins=6)
 plt.savefig('graphs/age_bins_hist.png')
+
+train[['Fare_bins']].hist(bins=4)
+plt.savefig('graphs/fare_bins_hist.png')
 
 train = train.drop(["PassengerId","Cabin","Ticket","Name","Fare","Age"], axis=1)
 test = test.drop(["PassengerId","Cabin","Ticket","Name","Fare","Age"], axis=1)
+
+print(train.describe())
+print(test.describe())
 
 plt.close
 
@@ -127,28 +151,12 @@ plt.savefig('graphs/heatmap.png')
 plt.close
 x=0
 
-#####deal with nan
+#####deal with nan embarked
+imputer = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+imputer.fit(train[['Embarked']])
+train[['Embarked']]= imputer.transform(train[['Embarked']])
 
-print(pd.isnull(train).sum() > 0)
-print(pd.isnull(test).sum() > 0)
-
-columns_nan = ('Age_bins','Embarked')
-
-for column in columns_nan:
-	if column == 'Embarked':
-		strat ='most_frequent'
-	else:
-		strat ='mean'
-	imputer = SimpleImputer(missing_values=np.nan, strategy=strat)
-	imputer.fit(train[[column]])
-	train[[column]]= imputer.transform(train[[column]])
-
-	imputer = SimpleImputer(missing_values=np.nan, strategy=strat)
-	imputer.fit(test[[column]])
-	test[[column]]= imputer.transform(test[[column]])
-
-#transform categorial to numerical 
-
+#####transform categorial to numerical 
 label_encoder = preprocessing.LabelEncoder() 
 
 train['Sex']= label_encoder.fit_transform(train['Sex']) 
@@ -157,16 +165,14 @@ train['Embarked']= label_encoder.fit_transform(train['Embarked'])
 test['Sex']= label_encoder.fit_transform(test['Sex']) 
 test['Embarked']= label_encoder.fit_transform(test['Embarked'])
 
-#define training and test sets
-
+#####define training and test sets
 X_train = train.drop("Survived", axis=1)
 Y_train = train["Survived"]
 X_test  = test.copy()
 Y_test = pd.read_csv("gender_submission.csv")
 Y_test = Y_test.drop("PassengerId", axis=1)
 
-#fit data
-
+#####fit data
 logreg = LogisticRegression(max_iter=1000)
 logreg.fit(X_train, Y_train)
 Y_pred = logreg.predict(X_test)
