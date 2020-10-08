574ba099e8881aff7664445302009a67e093c206
David Wöhlert <david.woehlert@gmx.de>
2020-10-08 22:50:30 +0200

    Cabin data, Optimize random forest hyperparameters

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from collections import Counter
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 from sklearn.impute import SimpleImputer
 from sklearn import preprocessing 
 from sklearn import metrics
@@ -16,8 +17,13 @@ train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 
 #####Fill empty cells with nan
-train['Age'] = train['Age'].replace('NaN', np.nan)
-test['Age'] = test['Age'].replace('NaN', np.nan)
+print(train.columns[train.isnull().any()].tolist())
+print(test.columns[test.isnull().any()].tolist())
+column_nan = ('Age','Cabin','Fare')
+for i in column_nan:
+	train[i] = train[i].replace('NaN', np.nan)
+	test[i] = test[i].replace('NaN', np.nan)
+
 
 #####Show data statistics
 print(train.describe())
@@ -39,7 +45,7 @@ titels = ('Master.','Dr.','Rev.','Major.','Mlle.','Col.','Don.','Mme.','Lady.','
 ######Fill empty numerical values and transform ticket into groups
 k=0
 for i in train['PassengerId']:
-	string = (train.iloc[k,3])
+	string = (train.loc[k,'Name'])
 	train.at[k, 'Name'] = string.split(',', 1)[0]
 	train.at[k, 'Titles'] = 0
 
@@ -48,14 +54,14 @@ for i in train['PassengerId']:
 			train.at[k, 'Titles'] = 1
 
 	if pd.isna(train.at[k, 'Age']):			###deal with unknown age
-		if train.iloc[k,6] < 2:
-			if train.iloc[k,7] > 2:
+		if train.loc[k,'SibSp'] < 2:
+			if train.loc[k,'Parch'] > 2:
 				train.at[k, 'Age'] = train.loc[train['Parch'] > 2, 'Age'].mean()
 			else:
 				train.at[k, 'Age'] = train['Age'].mean()
-		if train.iloc[k,6] == 2:
+		if train.loc[k,'SibSp'] == 2:
 			train.at[k, 'Age'] = train.loc[train['SibSp'] == 2, 'Age'].mean()
-		if train.iloc[k,6] > 2:
+		if train.loc[k,'SibSp'] > 2:
 			train.at[k, 'Age'] = train.loc[train['SibSp'] > 2, 'Age'].mean()
 
 	if pd.isna(train.at[k, 'Fare']):		###deal with unknown fare
@@ -67,18 +73,23 @@ for i in train['PassengerId']:
 			train.at[k, 'Fare'] = train.loc[train['Pclass'] == 3, 'Fare'].mean()
 
 	try:			####transform tickets into groups
-		integer = int(train.iloc[k,8])
+		integer = int(train.loc[k,'Ticket'])
 		if integer > 99999:
 			train.at[k, 'Ticket_group'] = 0
 		if integer < 100000:
 			train.at[k, 'Ticket_group'] = 1
 	except ValueError:
 		train.at[k, 'Ticket_group'] = 2
+
+	if pd.isna(train.at[k, 'Cabin']):
+		train.at[k, 'Cabin'] = 0
+	else:
+		train.at[k, 'Cabin'] = 1
 	k+=1
 
 k=0
 for i in test['PassengerId']:
-	string = (test.iloc[k,2])
+	string = (test.loc[k,'Name'])
 	test.at[k, 'Name'] = string.split(',', 1)[0]
 	test.at[k, 'Titles'] = 0
 
@@ -87,14 +98,14 @@ for i in test['PassengerId']:
 			test.at[k, 'Titles'] = 1
 
 	if pd.isna(test.at[k, 'Age']):		###deal with unknown age
-		if test.iloc[k,5] < 2:
-			if test.iloc[k,6] > 2:
+		if test.loc[k,'SibSp'] < 2:
+			if test.loc[k,'Parch'] > 2:
 				test.at[k, 'Age'] = train.loc[train['Parch'] > 2, 'Age'].mean()
 			else:
 				test.at[k, 'Age'] = train['Age'].mean()
-		if test.iloc[k,5] == 2:
+		if test.loc[k,'SibSp'] == 2:
 			test.at[k, 'Age'] = train.loc[train['SibSp'] == 2, 'Age'].mean()
-		if test.iloc[k,5] > 2:
+		if test.loc[k,'SibSp'] > 2:
 			test.at[k, 'Age'] = train.loc[train['SibSp'] > 2, 'Age'].mean()
 
 	if pd.isna(test.at[k, 'Fare']):		###deal with unknown fare
@@ -106,13 +117,18 @@ for i in test['PassengerId']:
 			test.at[k, 'Fare'] = train.loc[train['Pclass'] == 3, 'Fare'].mean()
 
 	try:		####transform tickets into groups
-		integer = int(test.iloc[k,7])
+		integer = int(test.loc[k,'Ticket'])
 		if integer > 99999:
 			test.at[k, 'Ticket_group'] = 0
 		if integer < 100000:
 			test.at[k, 'Ticket_group'] = 1
 	except ValueError:
 		test.at[k, 'Ticket_group'] = 2
+
+	if pd.isna(test.at[k, 'Cabin']):
+		test.at[k, 'Cabin'] = 0
+	else:
+		test.at[k, 'Cabin'] = 1
 	k+=1
 
 #####create bins for age and fare
@@ -134,17 +150,19 @@ plt.savefig('graphs/age_bins_hist.png')
 train[['Fare_bins']].hist(bins=4)
 plt.savefig('graphs/fare_bins_hist.png')
 
-train = train.drop(["PassengerId","Cabin","Ticket","Name","Fare","Age"], axis=1)
-test = test.drop(["PassengerId","Cabin","Ticket","Name","Fare","Age"], axis=1)
+train = train.drop(["PassengerId","Ticket","Name","Fare","Age"], axis=1)
+test = test.drop(["PassengerId","Ticket","Name","Fare","Age"], axis=1)
+
+train['Cabin']=pd.to_numeric(train['Cabin'])
+test['Cabin']=pd.to_numeric(test['Cabin'])
 
 print(train.describe())
 print(test.describe())
-
 plt.close
 
 trainMatrix = train.corr()
 
-f, ax = plt.subplots(figsize=(11, 9))
+f, ax = plt.subplots(figsize=(12, 10))
 
 sn.heatmap(trainMatrix, annot=True)
 plt.savefig('graphs/heatmap.png')
@@ -172,14 +190,36 @@ X_test  = test.copy()
 Y_test = pd.read_csv("gender_submission.csv")
 Y_test = Y_test.drop("PassengerId", axis=1)
 
-#####fit data
+#####fit data LogReg
 logreg = LogisticRegression(max_iter=1000)
 logreg.fit(X_train, Y_train)
 Y_pred = logreg.predict(X_test)
 acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
 print('Accuracy Linear Regression:',acc_log)
 
-clf=RandomForestClassifier(n_estimators=100)
-clf.fit(X_train,Y_train)
-Y_pred=clf.predict(X_test)
-print("Accuracy Random Forest:",metrics.accuracy_score(Y_test, Y_pred))
+#####fit data Random Forest
+clf_simple=RandomForestClassifier(n_estimators= 100)
+clf_simple.fit(X_train,Y_train)
+Y_pred=clf_simple.predict(X_test)
+print("Accuracy Simple Random Forest:",metrics.accuracy_score(Y_test, Y_pred))
+
+#####Optimize Hyperparameters
+# clf=RandomForestClassifier()
+# param_grid = { 
+#     'n_estimators': [200, 500],
+#     #'min_samples_split': [2, 5, 10, 15, 100],
+# 	#'min_samples_leaf': [1, 2, 5],
+#     'max_features': ['auto'], #, 'sqrt', 'log2'
+#     'max_depth' : [2,4,6,8],
+#     'criterion' :['gini'] #, 'entropy'
+# }
+# CV_clf = GridSearchCV(estimator=clf, param_grid=param_grid, cv= 5)
+# CV_clf.fit(X_train, Y_train)
+# print('Optimized Random Forest Classifier:')
+# print(CV_clf.best_params_)
+
+#####Optimized Random Forest
+clf_final=RandomForestClassifier(max_features='auto', n_estimators= 200, max_depth=4, criterion='gini')
+clf_final.fit(X_train,Y_train)
+Y_pred=clf_final.predict(X_test)
+print("Accuracy Optimized Random Forest:",metrics.accuracy_score(Y_test, Y_pred))
